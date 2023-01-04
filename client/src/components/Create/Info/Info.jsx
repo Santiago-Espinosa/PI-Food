d7df6a87bd13996a21b6e27d45629e754fe3d9b3
santiago666 <santiago666.sbe@gmail.com>
2023-01-03 23:54:41 -0500

    Details Push

--- client/src/components/Create/Info/Info.jsx
+++ client/src/components/Create/Info/Info.jsx
@@ -1,60 +1,78 @@
 import React, { Component } from "react";
-import style from './Info.module.css';
+import style from "./Info.module.css";
 
 class Info extends Component {
-    
-    render() {
+  render() {
+    const { name, image, healthScore, summary, changeHandler } = this.props;
 
-        const { name, image, healthScore, summary, changeHandler } = this.props
+    const { nameValidation, healthScoreValidation, summaryValidation } = this.props.validation;
 
-        const {nameValidation, healthScoreValidation, summaryValidation} = this.props.validation
+    return (
+      <div className={style.Info}>
+        <h3>Main Data</h3>
 
-        return (
-            <div className={style.Info}>
-                <h3>Datos principales</h3>
+        <label>
+          Name*
+          <input
+            className={style[`dato${!nameValidation ? `Error` : ""}`]}
+            name={"name"}
+            required
+            value={name}
+            onChange={changeHandler}
+            type="text"
+            autoComplete="off"
+          />
+          {!nameValidation ? (
+            <span className={style.textError}>You must enter your name</span>
+          ) : null}
+        </label>
 
-                <label >Nombre*
-                    <input className={style[`dato${!nameValidation ? `Error` : ""}`]} 
-                        name={"name"}
-                        required
-                        value={name}
-                        onChange={changeHandler}
-                        type="text"
-                        autoComplete="off" />
-                        {!nameValidation? <span className={style.textError}>Debe ingresar un nombre</span>:null}
-                </label>
+        <label>
+          Picture - URL
+          <input
+            className={style.dato}
+            name={"image"}
+            value={image}
+            onChange={changeHandler}
+            type="text"
+            autoComplete="off"
+          />
+        </label>
 
-                <label >URL-Imagen
-                    <input className={style.dato}
-                        name={"image"}
-                        value={image}
-                        onChange={changeHandler}
-                        type="text"
-                        autoComplete="off" />
-                </label>
+        <label>
+          Health Score*
+          <input
+            className={style[`dato${!healthScoreValidation ? `Error` : ""}`]}
+            name={"healthScore"}
+            value={healthScore}
+            onChange={changeHandler}
+            type="number"
+            autoComplete="off"
+          />
+          {!healthScoreValidation ? (
+            <span className={style.textError}>Min: 0 - Max: 100</span>
+          ) : null}
+        </label>
 
-                <label >Health Score*
-                    <input className={style[`dato${!healthScoreValidation ? `Error` : ""}`]} 
-                        name={"healthScore"}
-                        value={healthScore}
-                        onChange={changeHandler}
-                        type="number"
-                        autoComplete="off" />
-                        {!healthScoreValidation? <span className={style.textError}>Min: 0 - Max: 100</span>:null}
-                </label >
-
-                <label >Descripcion*
-                    <textarea  className={style[`dato${!summaryValidation ? `Error` : ""}`]}
-                        name={"summary"}
-                        value={summary}
-                        onChange={changeHandler}
-                        type="text"
-                        autoComplete="off" />
-                        {!summaryValidation? <span className={style.textError}>Debe ingresar una descripcion</span>:null}
-                </label>
-            </div>
-        )
-    }
+        <label>
+          Summary*
+          <textarea
+            className={style[`dato${!summaryValidation ? `Error` : ""}`]}
+            name={"summary"}
+            value={summary}
+            onChange={changeHandler}
+            type="text"
+            autoComplete="off"
+          />
+          {!summaryValidation ? (
+            <span className={style.textError}>
+              You must enter a description
+            </span>
+          ) : null}
+        </label>
+      </div>
+    );
+  }
 }
 
 export default Info;
--- client/src/components/Create/Steps/Steps.jsx
+++ client/src/components/Create/Steps/Steps.jsx
@@ -1,92 +1,82 @@
 import React, { useState, useEffect } from "react";
-import style from './Steps.module.css';
+import style from "./Steps.module.css";
 
 export default function Steps(props) {
+  const { currentSteps, addStep, deleteStep, resetStep, deleteStepByN } = props;
+  const [step, setStep] = useState("");
+  const [disabledAdd, setDisabledAdd] = useState(true);
+  const [disabledRD, setDisabledRD] = useState(false);
 
-    const { currentSteps, addStep, deleteStep, resetStep, deleteStepByN} = props;
-    const [step, setStep] = useState("");
-    const [disabledAdd, setDisabledAdd] = useState(true);
-    const [disabledRD, setDisabledRD] = useState(false);
+  useEffect(() => {
+    currentSteps.length ? setDisabledRD(false) : setDisabledRD(true);
+  }, [setDisabledRD, currentSteps]);
 
-    useEffect(() => {
-        
-        (currentSteps.length)? setDisabledRD(false): setDisabledRD(true)
+  useEffect(() => {
+    !/^\s*$/.test(step) ? setDisabledAdd(false) : setDisabledAdd(true);
+  }, [step]);
 
-    }, [setDisabledRD, currentSteps])
+  const changeHandler = (e) => {
+    setStep(e.target.value);
+  };
 
-    useEffect(() => {
+  const pressAdd = (e) => {
+    e.preventDefault();
 
-        ((!/^\s*$/.test(step)))?  setDisabledAdd(false): setDisabledAdd(true)
-
-    }, [step])
-
-    const changeHandler = (e) => {
-
-        setStep(e.target.value);
+    if (step !== "") {
+      addStep(step);
+      setStep("");
     }
+  };
 
-    const pressAdd = (e) => {
+  const pressReset = (e) => {
+    e.preventDefault();
+    resetStep();
+  };
 
-        e.preventDefault();
+  const pressDelete = (e) => {
+    e.preventDefault();
+    deleteStep();
+  };
 
-        if (step !== "") {
+  const pressDeleteStep = (e) => {
+    e.preventDefault();
 
-            addStep(step);
-            setStep("");
+    deleteStepByN(e.target.value);
+  };
 
-        }
-    }
+  return (
+    <div className={style.stepsConteiner}>
+      <label>
+        <h3>Steps</h3>
 
-    const pressReset = (e) => {
-        e.preventDefault();
-        resetStep()
-    }
-
-    const pressDelete = (e) => {
-        e.preventDefault();
-        deleteStep()
-    }
-
-    const pressDeleteStep = (e) => {
-        e.preventDefault();
-
-        deleteStepByN(e.target.value)
-
-    }
-
-    return (
-        <div className={style.stepsConteiner}>
-            <label >
-                <h3>Steps</h3>
-
-                <textarea className={style.textarea}
-                    value={step}
-                    onChange={changeHandler}
-                    onKeyDown={e => (e.key === "Enter") ? pressAdd(e) : null}
-                />
-                <div>
-                    <button className={`${style.button} ${!disabledRD? null : style.buttonDisable}`} onClick={pressReset}>Borrar Todo</button>
-                    <button className={`${style.button} ${!disabledRD? null : style.buttonDisable}`} disabled={disabledRD} onClick={pressDelete}>Borrar Ultima</button>
-                    <button className={`${style.button} ${!disabledAdd? null : style.buttonDisable}`} disabled={disabledAdd} onClick={pressAdd}>Agregar</button>
-                </div>
-
-            </label>
-
-            <div className={style.currentSteps}>
-                <h3>Step List:</h3>
-                <div >
-                    {
-                    currentSteps[0]? currentSteps.map(([key, value]) => <span key={key}>
-                                                                            <button className={style.buttonSteps} 
-                                                                                    value={key} 
-                                                                                    onClick={pressDeleteStep}>X
-                                                                            </button>
-                                                                            {key + " - " + value}
-                                                                        </span>):
-                                    <span>No hay pasos</span>
-                    }
-                </div>
-            </div>
+        <textarea
+          className={style.textarea}
+          value={step}
+          onChange={changeHandler}
+          onKeyDown={(e) => (e.key === "Enter" ? pressAdd(e) : null)}
+        />
+        <div>
+          <button className={`${style.button}${!disabledRD ? null : style.buttonDisable}`}onClick={pressReset}>Delet All</button>
+          <button className={`${style.button}${!disabledRD ? null : style.buttonDisable}`}disabled={disabledRD}onClick={pressDelete}>Delet Last</button>
+          <button className={`${style.button}${!disabledAdd ? null : style.buttonDisable}`}disabled={disabledAdd}onClick={pressAdd}>Add</button>
         </div>
-    )
-} 
+      </label>
+
+      <div className={style.currentSteps}>
+        <h3>Step List:</h3>
+        <div>
+          {currentSteps[0] ? (
+            currentSteps.map(([key, value]) => (
+              <span key={key}>
+                <button className={style.buttonSteps} value={key} onClick={pressDeleteStep}>X</button> 
+                {key + " - " + value}
+              </span>
+            ))
+          ) : (
+            <span>Indicate your steps for the creation of the recipe</span>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
